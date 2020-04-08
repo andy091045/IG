c965d6e16b2b354eb5aa21643e6551dc9abba78e
andy091045 <55223687+andy091045@users.noreply.github.com>
2020-04-08 09:23:40 +0800

    barcomplete

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
@@ -8,10 +8,37 @@ import DetailScreen from './src/screens/DetailScreen';
 
 const Stack = createStackNavigator();
 
+function LogoTitle() {
+  return (
+    <View style={styles.headerStyle}>
+      <Image
+        style={styles.camIconStyle}
+        source={require('./src/icon/icons8-webcam-100.png')}
+      />
+      <Image
+        style={{ width: 150, height: 42 }}
+        source={{ uri: 'https://www.instagram.com/static/images/web/mobile_nav_type_logo-2x.png/1b47f9d0e595.png' }}
+      />
+      <Image
+        style={styles.planeIconStyle}
+        source={require('./src/icon/icons8-paper-plane-100.png')}
+      />
+    </View>
+  );
+}
+
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
+        <Stack.Screen name="HomeHeader" component={AlbumScreen}
+          options={{
+            headerTitle: props => <LogoTitle {...props} />,
+            headerStyle: {
+              height: 90,
+            },
+          }}
+        />
         <Stack.Screen name="Home" component={AlbumScreen} />
         <Stack.Screen name="Detail" component={DetailScreen} />
       </Stack.Navigator>
@@ -20,10 +47,20 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  container: {
+  headerStyle: {
     flex: 1,
-    backgroundColor: '#fff',
+    flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'center',
+    // justifyContent: 'center',
+  },
+  camIconStyle: {
+    height: 35,
+    width: 35,
+    marginRight: 80,
+  },
+  planeIconStyle: {
+    height: 35,
+    width: 35,
+    marginLeft: 80,
   },
 });
